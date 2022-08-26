0b0874ea66078cc7ab99c23f206603e2ccb4d87a
smile-e3 <2278215957@qq.com>
2022-08-26 12:57:44 +0800

    新增:添加主题配置和其他部分配置

---
+++ code_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 安装Neovim
+
+
+# 安装
---
+++ theme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# == 安装轻量级桌面 ==
+sudo apt install xfce4 -y
+
+# == 安装主题以及ICONS ==
+tar -xf theme_icons/Dracula.tar.xz
+sudo mv theme_icons/Dracula /usr/share/themes
+tar -xf theme_icons/BigSur.tar.xz
+sudo mv theme_icons/BigSur /usr/share/icons
+sudo mv theme_icons/BigSur-dark /usr/share/icons
+
+# == 安装hack ttf字体 ==
+sudo apt-get -y install fonts-hack-ttf
+
+# == 上述主题需要先退出，切换为XFCE后在Application->Settings->Appearance->Style/Icons/Font进行配置 ==
+echo -e "\033[32m[INFO]请退出切换XFCE桌面后手动配置\033[0m"
